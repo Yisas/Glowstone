6958eb94b86412d7fb4c8bf8a3100ef797b3a2d3
meetaz <mshbli75@hotmail.com>
2018-02-25 17:58:38 -0500

    Before test Method for StopCommand class

---
+++ src/test/java/net/glowstone/command/minecraft/StopCommandTest.java
@@ -0,0 +1,49 @@
+package net.glowstone.command.minecraft;
+
+import static org.mockito.Mockito.when;
+import net.glowstone.GlowServer;
+import net.glowstone.command.CommandUtils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({Bukkit.class, CommandUtils.class, GlowServer.class})
+
+public class StopCommandTest {
+
+	private Command command;
+    private CommandSender sender, failedSender;
+
+    @Mock
+    GlowServer server;
+
+	@Before
+	public void before() throws Exception{
+		 command = new StopCommand();
+		 server = PowerMockito.mock(GlowServer.class);
+	     sender = PowerMockito.mock(CommandSender.class);
+	     failedSender = PowerMockito.mock(CommandSender.class);
+
+		 PowerMockito.mockStatic(Bukkit.class);
+		 when(Bukkit.getServer()).thenReturn(server);
+
+		 PowerMockito.when(sender.hasPermission(Mockito.anyString())).thenReturn(true);
+	     PowerMockito.when(sender.getServer()).thenReturn(server);
+
+	}
+
+
+
+}
+
+
+
