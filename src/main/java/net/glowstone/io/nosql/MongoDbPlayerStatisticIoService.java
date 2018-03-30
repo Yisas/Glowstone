947c0ae6ff7ae5afd6cc4d4c5b782999e0340a3f
Christopher Yap <cyap.mobile@gmail.com>
2018-03-29 20:31:31 -0400

    Saving data differently so I can read it back

--- src/main/java/net/glowstone/io/nosql/MongoDbPlayerStatisticIoService.java
+++ src/main/java/net/glowstone/io/nosql/MongoDbPlayerStatisticIoService.java
@@ -1,8 +1,12 @@
 package net.glowstone.io.nosql;
 
+import com.mongodb.Block;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+
+import java.util.Iterator;
 
 import org.bson.Document;
 import org.json.simple.JSONObject;
@@ -27,9 +31,42 @@ public class MongoDbPlayerStatisticIoService {
      */
     public void forklift(JSONObject json) {
         MongoCollection<Document> collection = database.getCollection("statistic");
-        String jsonstring = json.toString();
-        System.out.println(jsonstring);
-        Document document = new Document("chris", jsonstring);
+        
+        Document document = new Document("name", "chris");
+        
+        // iterate over json and save key value
+        for (Iterator ikeys = json.keySet().iterator(); ikeys.hasNext();) {
+            String key = (String) ikeys.next();
+            /*
+             * The json key has the format stat.<name>.
+             * When storing in mongo you cannot have the dot notation in the key.
+             * We are removing "stat." and just storing the real name of the key.
+             */
+            String newkey = key.toString().substring(5);
+            document.append(newkey, json.get(key));
+        }
+        
         collection.insertOne(document);
+        
+        readStat("chris");
     }
+    
+    /**
+     * Read from mongo.
+     *
+     */
+    public void readStat(String name) {
+        MongoCollection<Document> collection = database.getCollection("statistic");
+        collection.find(Filters.eq("name", name)).forEach(printBlock);
+    }
+    
+    /*
+     * print document.
+     */
+    private Block<Document> printBlock = new Block<Document>() {
+        @Override
+        public void apply(final Document document) {
+            System.out.println(document.toJson());
+        }
+    };
 }
