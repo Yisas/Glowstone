23779c92e190b00d307ff72f7c2ed7dea2b85263
Lenz Petion <da_haitian@hotmail.com>
2018-04-02 21:36:13 -0400

    Migration
    So forkLift -> checkInconsistency -> jsonObjsAreEqual(to check again  that all match )  -> write only to new Db

--- src/main/java/net/glowstone/io/nosql/MongoDbPlayerStatiscloServiceMigration.java
+++ src/main/java/net/glowstone/io/nosql/MongoDbPlayerStatiscloServiceMigration.java
@@ -8,11 +8,17 @@ import java.io.File;
 //import com.mongodb.MongoClient;
 //import com.mongodb.client.MongoDatabase;
 
+import org.bson.Document;
+
+import com.mongodb.client.MongoCollection;
+
 import net.glowstone.GlowServer;
 import net.glowstone.entity.GlowPlayer;
 import net.glowstone.io.json.JsonPlayerStatisticIoService;
 
 public class MongoDbPlayerStatiscloServiceMigration  extends JsonPlayerStatisticIoService {
+	
+	
 
     //To migrate the data from the old datastore to the new one you need to read from 
     // old data store make sure data is valid with constency cheker then write
@@ -24,7 +30,10 @@ public class MongoDbPlayerStatiscloServiceMigration  extends JsonPlayerStatistic
     private JsonPlayerStatisticIoService jsonP = 
             new JsonPlayerStatisticIoService(server, statsDir);
     private GlowPlayer player;
+    
+    
 
+   
     /**
      * Constructor.
      */
@@ -38,9 +47,30 @@ public class MongoDbPlayerStatiscloServiceMigration  extends JsonPlayerStatistic
      * Migration.
      */
     public void migration() {
+    	Boolean pass = false;
+    	mongoP.forklift(player);
     	
+    	// this will read write old and new db and set a inconsistence value
+    	//if it reach a certain value it set migration instance to ready
+    int x =	mongoP.checkInconsistency(player);
+   
+    	//This will run the check again and if it true  both data match
+    	for(int i = 0 ; i< x ;i++) {
+    		pass = mongoP.jsonObjsAreEqual(player);
+		}
+    	
+    	if(MigrationSingleton.getInstance().getMigrationValue() == "Ready" && pass ) {
+    		
+    		//Write only to new DB	
+    		mongoP.writeStatistics(player);
+    		
+    	}
+    	
+  
 
 
     }
+    
+    
 
 }
